02f8f000098c7aba33755f3f91c29a8415333ff5
Jinjin123 <1293813551@qq.com>
2017-05-12 23:24:07 +0800

    updating

--- v7.8.0_v1/docker-entrypoint.sh
+++ v7.8.0_v1/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export PATH=$PATH:/usr/local/.nvm/versions/node/v7.8.0/bin
-export NEW_RELIC_NO_CONFIG_FILE=True
-export NEW_RELIC_APP_NAME=$NEWRELICAPPNAME
-export NEW_RELIC_LICENSE_KEY=$NEWRELICKEY
+#export NEW_RELIC_NO_CONFIG_FILE=True
+#export NEW_RELIC_APP_NAME=$NEWRELICAPPNAME
+#export NEW_RELIC_LICENSE_KEY=$NEWRELICKEY
 CODE_PATH='/var/www/html'
 MONITOR='/var/www'
 /bin/cp $MONITOR/newrelic.js  $CODE_PATH/newrelic.js
@@ -16,6 +16,7 @@ then
         #/bin/sed -i '1 irequire ("newrelic")' $PATH/consume/consume.js
         cd $CODE_PATH && /usr/local/.nvm/versions/node/v7.8.0/bin/node  consume/consume.js
 else
+	/bin/sed -ie '/newrelic/d' $CODE_PATH/bin/www
         /bin/sed -i '1 irequire ("newrelic")' $CODE_PATH/bin/www
-        cd $CODE_PATH && /usr/local/.nvm/versions/node/v7.8.0/bin/node  bin/www
+        cd $CODE_PATH && /usr/local/.nvm/versions/node/v7.8.0/bin/npm  install  newrelic &&  /usr/local/.nvm/versions/node/v7.8.0/bin/node  bin/www
 fi
