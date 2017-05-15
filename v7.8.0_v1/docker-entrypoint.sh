906feb4ac430cd452e29e6b0ed6baaede116d3c6
Jinjin123 <1293813551@qq.com>
2017-05-15 13:29:49 +0800

    updating

--- v7.8.0_v1/docker-entrypoint.sh
+++ v7.8.0_v1/docker-entrypoint.sh
@@ -7,8 +7,8 @@ CODE_PATH='/var/www/html'
 MONITOR='/var/www'
 /bin/cp $MONITOR/newrelic.js  $CODE_PATH/newrelic.js
 cd $CODE_PATH
-#/bin/sed -i "s/app_name: .*/app_name: [\"$NEWRELICAPPNAME\"],/"   $CODE_PATH/newrelic.js
-#/bin/sed -i "s/license_key: .*/license_key: \"$NEWRELICKEY\",/"   $CODE_PATH/newrelic.js
+/bin/sed -i "s/app_name: .*/app_name: [\"$NEWRELICAPPNAME\"],/"   $CODE_PATH/newrelic.js
+/bin/sed -i "s/license_key: .*/license_key: \"$NEWRELICKEY\",/"   $CODE_PATH/newrelic.js
 if [ -e $CODE_PATH/server/server.js ]
 then
         #/bin/sed -i '1 irequire ("newrelic")' $CODE_PATH/server/server.js
@@ -18,5 +18,11 @@ then
 else
 	/bin/sed -ie '/newrelic/d' $CODE_PATH/bin/www
         /bin/sed -i '1 irequire ("newrelic")' $CODE_PATH/bin/www
-        cd $CODE_PATH && /usr/local/.nvm/versions/node/v7.8.0/bin/npm  install && /usr/local/.nvm/versions/node/v7.8.0/bin/npm  install  newrelic &&  /usr/local/.nvm/versions/node/v7.8.0/bin/node  bin/www
+        cd $CODE_PATH && /usr/local/.nvm/versions/node/v7.8.0/bin/npm  install
+        if [ $? -ne 0 ]
+        then
+                /usr/local/.nvm/versions/node/v7.8.0/bin/npm  install  newrelic && /usr/local/.nvm/versions/node/v7.8.0/bin/node  bin/www
+        else
+                /usr/local/.nvm/versions/node/v7.8.0/bin/npm  install  newrelic && /usr/local/.nvm/versions/node/v7.8.0/bin/node  bin/www
+        fi
 fi
